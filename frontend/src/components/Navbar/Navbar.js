f0ab8a247e8bd33e5198e65534bc5a6042b99ad9
Prasad Jawale <80669588+Tydos@users.noreply.github.com>
2024-03-24 15:43:04 +0530

    navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -10,10 +10,20 @@ const Navbar = () => {
     await searching(searchValue);
   };
   return (
-    <div className="w-4/5 flex items-center justify-center h-32 bg-grey-100 rounded-full">
+    <div className="flex items-center justify-center p-4 bg-gray-300 rounded-full" style={{ width: '95vw' }}>
       <div className="w-full max-w-screen-xl text-white">
         <div className="flex items-center justify-between mx-auto h-full px-4">
-          <div>A</div>
+          <div className="flex items-center">
+          <img
+              src="https://res.cloudinary.com/duws62b88/image/upload/v1711273075/logo_itrsjo.svg"
+              alt="logo"
+              className="w-12 h-12 object-cover rounded-full cursor-pointer mt-1"
+            />
+            <div class="text-gray-900">
+  <p class="text-2xl font-bold px-3" >DocSpot</p>
+</div>
+            </div>
+            
           <div className="flex items-center">
             {/* Text field for search */}
             <input
@@ -21,7 +31,7 @@ const Navbar = () => {
               value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search"
-              className="mr-2 px-2 py-1 rounded border border-gray-400 text-grey-100"
+              className="mr-2 px-2 py-1 rounded border border-gray-400 text-grey-200" style={{ width: '45vw' }}
             />
             {/* Button to trigger search */}
             <button
@@ -30,7 +40,10 @@ const Navbar = () => {
             >
               Search
             </button>
+            <div className="bg-blue-500 mx-5 text-white px-3 py-1 rounded">
             <div>Login</div>
+            </div>
+            
           </div>
         </div>
       </div>
